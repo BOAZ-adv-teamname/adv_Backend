30b84affe643888128817cfa3f5a8c7fb2d78e75
Kim Minyoung <61506233+kkminyoung@users.noreply.github.com>
2021-07-25 16:03:32 +0900

    add news preprocessing

--- similarity_service/model/precedent_dao.py
+++ similarity_service/model/precedent_dao.py
@@ -4,13 +4,24 @@ from pymongo import MongoClient
 import json
 import pandas as pd
 import numpy as np
+import re
 
 import os
+
+def read_news_data(path):
+    news_data = list(pd.read_csv(path))[0]
+    string = re.sub("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]"," ", news_data)
+    string = re.sub(r'[·@%\\*=()/~#&\+á‘’“”?\xc3\xa1\-\|\:\;\!\-\,\_\~\$\'\"\[\]]', ' ', string) #remove punctuation
+    string = re.sub(r'\n',' ', string)     # remove enter
+    string = re.sub(r'[0-9]+', '', string) # remove number
+    string = re.sub(r'\s+', ' ', string)   #remove extra space
+    cleaned_news_data = re.sub(r'<[^>]+>',' ',string) #remove Html tags
+    return cleaned_news_data
 
 def get_precedent_data(local):
     # local csv 파일
     if local:
-        sum_database = pd.read_csv('./model/Sum_Database/0706_KoBart512.csv')
+        sum_database = pd.read_csv('./model/Sum_Database/concat.csv')
     # mongo db
     else:
         try:
@@ -20,7 +31,7 @@ def get_precedent_data(local):
             sum_database = collection.find({})
         except:
             print('mongo db connect error')
-            sum_database = pd.read_csv(path)
+            sum_database = pd.read_csv('./model/Sum_Database/concat.csv')
 
     sum_database=sum_database[sum_database.kobart_sum.notna()]
     return sum_database.kobart_sum.values.tolist()
